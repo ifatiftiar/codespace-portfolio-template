4471dec14cea48a33f937da002bb53ba4fba1e81
Kazi Ifat Iftiar Fahim <87182697+ifatiftiar@users.noreply.github.com>
2024-08-21 18:30:13 +0000

    chore: Update package.json and homepage URL

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,20 +24,13 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Chemical Engineering student at the Bangladesh University of Engineering and Technology who is passionate about technology and its applications. I am looking forward to dive deep in machine learning in future.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["HTML", "CSS", "Javascript", "React", "Python", "C++", "Video Editing"];
 
 /**
  * Use this to give more information about what you are passionate about,
@@ -45,7 +38,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I'm passionate about solving real world problems through coding. I want to contribute to open source projects and make a positive impact on the world.";
 
 const About = () => {
   return (
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/ifat-iftiar.png";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Ifat Iftiar in a blue panjani, sitting on a low wall and a smile in his face";
 
 const Home = ({ name, title }) => {
   return (
